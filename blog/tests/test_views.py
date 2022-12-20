56396bb133d8cb4d6825c84af6a038396fb515a4
ALEX <you@example.com>
2022-12-21 00:14:58 +0300

    add test empty post

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,6 +1,22 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+
+# reverse('post_list')
+# reverse('post_detail')
+# reverse('post_new')
+# reverse('post_edit')
+from blog.models import Post
+
+
 class ViewsTestCase(TestCase):
-    def test_index_loads_properly(self):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(username='testuser', password='12345')
+        post = Post.objects.create(author=user, title='мой пост', text='мой текст')
+
+    def test_empty_post(self):
         """The index page loads properly"""
-        response = self.client.get('your_server_ip:8000')
+        response = self.client.get(reverse('post_list'))
         self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['posts'])
