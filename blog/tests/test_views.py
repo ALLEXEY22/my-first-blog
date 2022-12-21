6192728c704f342c3087c8f19a3b8538b4c734a7
ALEX <you@example.com>
2022-12-21 12:47:35 +0300

    добавил одну строчку в test_views.py

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -6,17 +6,23 @@ from django.urls import reverse
 # reverse('post_detail')
 # reverse('post_new')
 # reverse('post_edit')
+from django.utils.timezone import now
+
 from blog.models import Post
+from datetime import timedelta
 
 
 class ViewsTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         user = User.objects.create_user(username='testuser', password='12345')
-        post = Post.objects.create(author=user, title='мой пост', text='мой текст')
+        cls.post1 = Post.objects.create(author=user, title='мой пост', text='мой текст')
+        cls.post2 = Post.objects.create(author=user, title='мой пост', text='мой текст',
+                                        published_date=now() - timedelta(hours=1))
 
     def test_empty_post(self):
         """The index page loads properly"""
         response = self.client.get(reverse('post_list'))
         self.assertEqual(response.status_code, 200)
-        self.assertFalse(response.context['posts'])
+        self.assertFalse(response.context['posts'].filter(id=self.post1.id))
+        self.assertTrue(response.context['posts'].filter(id=self.post2.id))
